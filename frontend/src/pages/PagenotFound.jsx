e636256173c7ec4c26a168f3db19e21652449303
bhautik <bhautikjani1102@gmail.com>
2023-06-13 13:09:38 +0530

    ui updated 2

--- frontend/src/pages/PagenotFound.jsx
+++ frontend/src/pages/PagenotFound.jsx
@@ -3,9 +3,10 @@ import Header from "../components/Header";
 
 const PagenotFound = () => {
   return (
-    <div className="m-4">
+    <div className="-mt-4 pt-4 bg-gray-950">
       <Header />
-      <div className="flex justify-center">
+
+      <div className="flex justify-center bg-white">
         <img
           src="https://img.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_114360-1900.jpg"
           alt="Page Not Found! 404"
--- frontend/src/store/place.js
+++ frontend/src/store/place.js
@@ -28,3 +28,63 @@ const hostedPlaceSlice = createSlice({
 
 export const hostedPlaceActions = hostedPlaceSlice.actions;
 export default hostedPlaceSlice.reducer;
+
+/*
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAllPlacesAsync = createAsyncThunk(
+  "place/getAllPlaces",
+  async () => {
+    // Make an API request to log in the user and obtain the token
+    // console.log(email, password);
+    const response = await axios.post(`place/hostPlaces`);
+    console.log(response);
+    console.log(
+      "----------------------------------------->"
+      // response.data.user
+    );
+    return;
+    // return response.data.user;
+    // if (!response.ok) {
+    //   throw new Error("Login failed");
+    // }
+  }
+);
+
+const initialState = { data: null };
+
+// const InitialState = { yourHostedPlace: [], allHostedPlaces: null };
+
+const hostedPlaceSlice = createSlice({
+  name: "place",
+  initialState: initialState,
+  reducers: {
+    
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllPlacesAsync.pending, (state) => {
+        // Update the state to indicate that login is in progress
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(getAllPlacesAsync.fulfilled, (state, action) => {
+        // Update the state with the user data received from the API
+        state.loading = true;
+        state.data = { ...state.payload };
+      })
+      .addCase(getAllPlacesAsync.rejected, (state, action) => {
+        // Update the state to handle the login failure
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const hostedPlaceActions = hostedPlaceSlice.actions;
+export default hostedPlaceSlice.reducer;
+
+
+*/
